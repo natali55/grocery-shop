aa6b8b9c4e9a5e760af9c258ee93893f403ce970
Natalia_Mekh <natashamekh55@gmail.com>
2024-04-19 10:46:10 +0200

    (feat): add Access-Control-Allow-Credentials headers

--- infra/lib/product-service-stack.ts
+++ infra/lib/product-service-stack.ts
@@ -45,9 +45,9 @@ export class ProductServiceStack extends Stack {
         {
           statusCode: '200',
           responseParameters: {
-            'method.response.header.Access-Control-Allow-Headers': "'Content-Type'",
+            'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
             'method.response.header.Access-Control-Allow-Origin': "'https://d2ju856t2gddwd.cloudfront.net'",
-            'method.response.header.Access-Control-Allow-Credentials': "'false'",
+            'method.response.header.Access-Control-Allow-Credentials': "'true'",
             'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
           },
           responseTemplates: {
@@ -67,6 +67,7 @@ export class ProductServiceStack extends Stack {
         responseParameters: {
           'method.response.header.Access-Control-Allow-Headers': true,
           'method.response.header.Access-Control-Allow-Origin': true,
+          'method.response.header.Access-Control-Allow-Credentials': true,
           'method.response.header.Access-Control-Allow-Methods': true,
         }
       }],
@@ -83,8 +84,9 @@ export class ProductServiceStack extends Stack {
         {
           statusCode: '200',
           responseParameters: {
-            'method.response.header.Access-Control-Allow-Headers': "'Content-Type'",
+            'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
             'method.response.header.Access-Control-Allow-Origin': "'https://d2ju856t2gddwd.cloudfront.net'",
+            'method.response.header.Access-Control-Allow-Credentials': "'true'",
             'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
           },
           responseTemplates: {
@@ -102,6 +104,7 @@ export class ProductServiceStack extends Stack {
         responseParameters: {
           'method.response.header.Access-Control-Allow-Headers': true,
           'method.response.header.Access-Control-Allow-Origin': true,
+          'method.response.header.Access-Control-Allow-Credentials': true,
           'method.response.header.Access-Control-Allow-Methods': true
         }
       }]
