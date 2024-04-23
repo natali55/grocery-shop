f7b6d047c52bd33042e91b96650a41ef82338497
Natalia_Mekh <natashamekh55@gmail.com>
2024-04-23 18:12:42 +0200

    (feat): update environments in front-end app

--- front-end-app/src/environments/environment.prod.ts
+++ front-end-app/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://fd0mfl15q5.execute-api.us-east-1.amazonaws.com/prod',
+    bff: 'https://bl3nb9judc.execute-api.us-east-1.amazonaws.com/prod',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
