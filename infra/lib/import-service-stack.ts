c6d26e03ae66daeb964d62a9fe90ba61a0dbad90
Natalia_Mekh <natashamekh55@gmail.com>
2024-05-06 22:10:37 +0200

    (feat): task-5 fix

--- infra/lib/import-service-stack.ts
+++ infra/lib/import-service-stack.ts
@@ -103,7 +103,7 @@ export class ImportServiceStackUnique extends Stack {
                 statusCode: '200',
                 responseParameters: {
                     'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-                    'method.response.header.Access-Control-Allow-Origin': "'*'",
+                    'method.response.header.Access-Control-Allow-Origin': "'https://d2ju856t2gddwd.cloudfront.net'",
                     'method.response.header.Access-Control-Allow-Methods': "'GET,PUT,OPTIONS'"
                 }
             }],
